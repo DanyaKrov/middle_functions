ef77f112ec5ecd7187b4e6712ce5e1699cc682ab
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-11 12:42:01 +0300

    Update itc_multi_num.cpp

--- itc_multi_num.cpp
+++ itc_multi_num.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int itc_multi_num(long long number)
 {
+    if (number < 0)
+        number *= -1;
     int sum = number % 10;
     while (number >= 10){
         number /= 10;
