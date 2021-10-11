0b33b5979367198409994b338aeb573626be0446
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-11 12:44:53 +0300

    Update itc_rev_num.cpp

--- itc_rev_num.cpp
+++ itc_rev_num.cpp
@@ -5,5 +5,7 @@ using namespace std;
 
 int itc_rev_num(long long number)
 {
-    return itc_len_num(itc_pal_num(number)) + 1;
+    if (number < 0)
+        number *= -1;
+    return itc_len_num(itc_pal_num(number));
 }
