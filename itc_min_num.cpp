669ba3fca987bf04ae16225d30cc45522afe1935
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-11 12:43:46 +0300

    Update itc_min_num.cpp

--- itc_min_num.cpp
+++ itc_min_num.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int itc_min_num(long long number)
 {
+    if (number < 0)
+        number *= -1;    
     int max1 = number % 10;
     while (number >= 10){
         number /= 10;
