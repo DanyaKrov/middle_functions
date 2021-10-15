590e9f8564ce82a2fa1cac38cd18c1b84adbe5fe
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-15 10:35:46 +0300

    Update itc_max_num.cpp

--- itc_max_num.cpp
+++ itc_max_num.cpp
@@ -10,3 +10,8 @@ int itc_max_num(long long number)
     int max1 = number % 10;
     while (number >= 10){
         number /= 10;
+        if (max1 < number % 10)
+            max1 = number % 10;
+    }
+    return max1;
+}
