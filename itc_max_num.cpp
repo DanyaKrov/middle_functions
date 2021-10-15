0024db072bdd8f8653eb5ef1e48c78b208fa8b24
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-15 10:25:35 +0300

    Update itc_max_num.cpp

--- itc_max_num.cpp
+++ itc_max_num.cpp
@@ -3,15 +3,17 @@
 
 using namespace std;
 
-int itc_max_num(long long number)
+int itc_second_max_num(long long number)
 {
     if (number < 0)
         number *= -1;
-    int max1 = number % 10;
-    while (number >= 10){
-        number /= 10;
-        if (max1 < number % 10)
-            max1 = number % 10;
+    if (itc_len_num(number) == 1)
+        return -1;
+    int x = itc_max_num(number);
+    for (long long i = 1; i < number * 10; i = i * 10){
+        if (number % (i * 10) / i == x){
+            number -= x * i;
+            return itc_max_num(number);
+        }
     }
-    return max1;
 }
