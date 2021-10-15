f69d058b7721ae0c9731aae23ec68f20a878d29b
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-15 10:11:47 +0300

    Update itc_mirror_count.cpp

--- itc_mirror_count.cpp
+++ itc_mirror_count.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int itc_mirror_count(long long number)
 {
     int i = 1, count = 0;
-    while (i < number){
+    while (i <= number){
             int num = itc_pal_num(i);
             if (num == i){count ++;}
             i ++;
