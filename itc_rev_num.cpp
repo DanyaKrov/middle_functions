7d49939e5aa62c20d525cc7b5ef7910219b03898
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-15 09:41:12 +0300

    Update itc_rev_num.cpp

--- itc_rev_num.cpp
+++ itc_rev_num.cpp
@@ -5,6 +5,8 @@ using namespace std;
 
 int itc_rev_num(long long number)
 {
+    if (number >= 9223372036854775807 || number >= -9223372036854775808)
+        return -1;
     if (number < 0)
         number *= -1;
     return itc_len_num(itc_pal_num(number));
