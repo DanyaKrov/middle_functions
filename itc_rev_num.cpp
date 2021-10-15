9ad9f1af6ac33b8177acdaf1188b210fe4f637fd
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2021-10-15 10:31:23 +0300

    Update itc_rev_num.cpp

--- itc_rev_num.cpp
+++ itc_rev_num.cpp
@@ -6,8 +6,6 @@ using namespace std;
 int itc_rev_num(long long number)
 {
     int i = 0;
-    if (number == 9223372036854775808 || number == -9223372036854775809)
-        return -1;
     if (number < 0)
         number *= -1;
     for (long long num = number; number % 10 == 0; num = num / 10){
